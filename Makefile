78c19d38942809ce8694c167569144c3e1f06bdb
Brandon Willett <bcwillett1@gmail.com>
2018-03-31 13:09:56 -0400

    added lock for standard output, also nearly working histo_private

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 all: histogram
 
 histogram: histogram.cpp ppmb_io.a
-	gcc -O3 $^ -o $@ -lm
+	g++ -O3 $^ -o $@ -lm
 
 histo_private: histo_private.cpp ppmb_io.a
-	gcc -O3 $^ -o $@ -lm
+	g++ -O3 -std=c++11 -pthread $^ -o $@ -lm -pthread
 
 ppmb_io.a: ppmb_io.o 
 	ar rs $@ $<
--- histo_private.cpp
+++ histo_private.cpp
@@ -7,6 +7,15 @@
 #include <thread>
 using std::thread;
 
+#include <atomic>
+using std::atomic;
+
+#include <string>
+#include <sstream>
+#include <iostream>
+using std::cout;
+using std::endl;
+
 extern "C" {
 #include "ppmb_io.h"
 }
@@ -26,6 +35,9 @@ struct hists {
   int* hist_b;
 };
 
+const int UNLOCKED = 0;
+const int LOCKED = 1;
+
 void print_histogram(FILE *f, int *hist, int N) {
   fprintf(f, "%d\n", N+1);
   for(int i = 0; i <= N; i++) {
@@ -33,16 +45,41 @@ void print_histogram(FILE *f, int *hist, int N) {
   }
 }
 
-void histogram(struct img *input, int firstRow, int lastRow, struct hists h) {
+atomic<int> standardOutput(UNLOCKED);
+
+void acquire(atomic<int>* b)
+{
+  while(true)
+  {
+    int expected = UNLOCKED;
+    if ((*b).compare_exchange_weak(expected, LOCKED)) break;
+  }
+  // cout << "Sucessfully set b which is now " << (*b) << endl;
+  return;
+}
+
+void release(atomic<int>* b)
+{
+  // cout << "Going to release b which is now " << (*b) << endl;
+  (*b).store(UNLOCKED);
+  return;
+}
+
+void histogram(struct img *input, int firstRow, int lastRow, struct hists *h) {
   // we assume hist_r, hist_g, hist_b are zeroed on entry.
 
-  int start = firstRow * (*input).ysize;
-  int end = lastRow * (*input).ysize;
+  int start = firstRow * ((*input).ysize - 1) + 1;
+  int end = lastRow * ((*input).ysize - 1) + 1;
+
+  acquire(&standardOutput);
+  cout << "First pixel is " << start << endl;
+  cout << "Last pixel is " << end << endl;
+  release(&standardOutput);
 
   for(int pix = start; pix < end; pix++) {
-    h.hist_r[input->r[pix]] += 1;
-    h.hist_g[input->g[pix]] += 1;
-    h.hist_b[input->b[pix]] += 1;
+    (*h).hist_r[input->r[pix]] += 1;
+    (*h).hist_g[input->g[pix]] += 1;
+    (*h).hist_b[input->b[pix]] += 1;
   }
 }
 
@@ -76,11 +113,19 @@ int main(int argc, char *argv[]) {
     int N = input.xsize;
     int chunk_size = (int) ((N / threads) + 1);
 
+    cout << "N being xsize is uh " << N << endl;
+
     int start = 0;
     int end = chunk_size;
 
-    thread threadArray[threads];
-    struct hists resultHists[threads];
+    thread* threadArray = new thread[threads];
+    struct hists* resultHists = new struct hists[threads];
+
+    // Run the thing! One chunk for each thread
+    // ------------------------------------------------------------------------
+
+    // ggc::Timer t("histogram");
+    // t.start();
 
     for (int i = 0; i < threads; i++)
     {
@@ -90,9 +135,10 @@ int main(int argc, char *argv[]) {
         h.hist_g = (int *) calloc(input.maxrgb+1, sizeof(int));
         h.hist_b = (int *) calloc(input.maxrgb+1, sizeof(int));
 
-        threadArray[i] = thread(histogram, input, start, end, h);
+        threadArray[i] = thread(histogram, &input, start, end, &h);
+        // cout << "One chunk size being worked on is " << start << " < chunk < " << end << "not inclusive" << endl;
         start = start + chunk_size;
-        end = (N > end + chunk_size) ? (end + chunk_size) : N;
+        end = (N > end + chunk_size) ? (end + chunk_size) : N + 1;
 
         resultHists[i] = h;
     }
@@ -102,24 +148,36 @@ int main(int argc, char *argv[]) {
         threadArray[i].join();
     }
 
-    // ggc::Timer t("histogram");
-
-    // t.start();
-    // histogram(&input, hist_r, hist_g, hist_b);
+    cout << "finished" << endl;
     // t.stop();
 
+    // Combine the output
+    // ------------------------------------------------------------------------
 
+    struct hists final;
+    final.hist_r = (int *) calloc(input.maxrgb+1, sizeof(int));
+    final.hist_g = (int *) calloc(input.maxrgb+1, sizeof(int));
+    final.hist_b = (int *) calloc(input.maxrgb+1, sizeof(int));
 
+    for (int i = 0; i < threads; i++)
+    {
+        for (int j = 0; j < input.maxrgb+1; ++j)
+        {
+          final.hist_r[j] += resultHists[i].hist_r[j];
+          final.hist_g[j] += resultHists[i].hist_g[j];
+          final.hist_b[j] += resultHists[i].hist_b[j];
+        }
+    }
 
-    // FILE *out = fopen(output_file, "w");
-    // if(out) {
-    //   print_histogram(out, hist_r, input.maxrgb);
-    //   print_histogram(out, hist_g, input.maxrgb);
-    //   print_histogram(out, hist_b, input.maxrgb);
-    //   fclose(out);
-    // } else {
-    //   fprintf(stderr, "Unable to output!\n");
-    // }
+    FILE *out = fopen(output_file, "w");
+    if(out) {
+      print_histogram(out, final.hist_r, input.maxrgb);
+      print_histogram(out, final.hist_g, input.maxrgb);
+      print_histogram(out, final.hist_b, input.maxrgb);
+      fclose(out);
+    } else {
+      fprintf(stderr, "Unable to output!\n");
+    }
     // printf("Time: %llu ns\n", t.duration());
   }  
 }
